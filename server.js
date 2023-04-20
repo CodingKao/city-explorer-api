43139dce5f9572c42d4081655c37e4fdabcee545
Kao Saelor <saelor.kao@gmail.com>
2023-04-19 21:41:05 -0700

    intial commit with unsplash done

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 
+
+const axios = require('axios');
+
 // set variable for weather.json
 const weatherData = require('./data/weather.json');
 
@@ -33,49 +36,103 @@ app.listen(PORT, () => console.log(`Yay we are up on port ${PORT}`));
 app.get('/', (request, response) => {
   response.status(200).send('Welcome to my server!');
 });
-console.log('response');
+console.log('Welcome to my server!');
 
-// Get user name from parameter input
-// app.get('/hello', (request, response) => {
-//   let firstName = request.query.firstName;
-//   let lastName = request.query.lastName;
 
-//   console.log(request.query);
+// ***** Movie ***** //
 
-//   // prompt welcome message
-//   response.status(200).send(`Hello ${firstName} ${lastName}, welcome to my server`);
-// });
+app.get('/movies', async (req, res, next) => {
+  try {
+    let city = req.query.city;
+    // let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}`;
+    // console.log(url);
+    // let response = await axios.get(url);
+    // console.log(response.data);
+    // let movies = response.data.results.map(movie => new Movie(movie));
+    // res.status(200).send(movies);
+    
+    
+    // check if endpoint 'movies' works on Thunder
+    res.status(200).send(city);
 
-app.get('/weather', (req, res, next) => {
-  console.log('Weather endpoint hit');
-  console.log('All weather data:', weatherData);
+
+  } catch (error) {
+    next(error);
+  }
+});
+
+class Movie {
+  constructor(movieObj) {
+    this.title = movieObj.title;
+    this.poster = movieObj.poster_path;
+    this.releaseDate = movieObj.release_date;
+    this.overview = movieObj.overview;
+    this.voteAverage = movieObj.vote_average;
+    this.voteCount = movieObj.vote_count;
+    this.id = movieObj.id;
+  }
+}
+
+app.get('/photos', async (req, res, next) => {
+  try {
+    let cityImg = req.query.city;
+    let url = `https://api.unsplash.com/search/photos?client_id=${process.env.UNSPLASH_API_KEY}&query=${cityImg}`;
+    let imgResponse = await axios.get(url);
+    let dataSend = imgResponse.data.results.map(obj => new Photo(obj));
+    res.status(200).send(dataSend);
+
+    // check if endpoint 'photos' works on Thunder
+    // res.status(200).send(cityImg);
+
+  } catch (error) {
+    next(error);
+  }
+});
+
+class Photo {
+  constructor(imgObj) {
+    this.src = imgObj.urls.regular;
+    this.alt = imgObj.alt_description;
+    this.userName = imgObj.user.name;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ***** Weather ***** //
+app.get('/weather', async (req, res, next) => {
   try {
     let lat = parseFloat(req.query.lat);
     let lon = parseFloat(req.query.lon);
     let searchQuery = req.query.searchQuery;
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHERBIT_API_KEY}&query=${searchQuery}&lat=${lat}&lon=${lon}&days=16`;
+    
 
-    console.log('lat:', lat, 'lon:', lon, 'searchQuery:', searchQuery);
+    // check if endpoint 'weather' works on Thunder lat 47.60621
+    res.status(200).send(lat);
+    console.log('URL:', url); // request URL
 
-    let foundWeather = weatherData.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase() ||
-      Math.abs(parseFloat(city.lat) - parseFloat(lat)) < 0.01 ||
-      Math.abs(parseFloat(city.lon) - parseFloat(lon)) < 0.01);
+    let weatherResponse = await axios.get(url)
+    let forecasts = weatherResponse.data.data.map(weatherData => new Forecast(weatherData));
 
-
-    console.log('foundWeather:', foundWeather);
-
-
-    // **** Trying to understand why this original code is not working ****
-    // let lat = req.query.lat;
-    // let lon = req.query.lon;
-    // let searchQuery = req.query.searchQuery;
-
-    // let foundWeather = weatherData.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase() && city.lat == lat && city.lon == lon);
-
-    if (!foundWeather) {
-      return res.status(404).send('No weather found');
-    }
-
-    let forecasts = foundWeather.data.map(weatherData => new Forecast(weatherData));
+    console.log('Forecasts:', forecasts); // mapped forecasts
 
     res.status(200).send(forecasts);
   } catch (error) {
